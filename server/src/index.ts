0d81aea46c9a9341e56baff3dfbaa344e3df75da
Joice-crypto <joyciinha_cristina@aluno.ufsj.edu.br>
2023-11-23 20:19:29 -0300

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+
+
+const app = fastify();
+
+app.register(cors, {
+    origin: true,
+})
+
+app.listen({ port: 3333  }).then(() => {
+    console.log('Server is runing')
+})
